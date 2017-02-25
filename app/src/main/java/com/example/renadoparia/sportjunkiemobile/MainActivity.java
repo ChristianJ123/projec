ebbd29ae5a71ed1ee6d5dde8a487dbd133c8068d
renadope <renadoparia@gmail.com>
2017-02-25 14:48:46 -0400

    updated Constraint Layout

--- app/src/main/java/com/example/renadoparia/sportjunkiemobile/MainActivity.java
+++ app/src/main/java/com/example/renadoparia/sportjunkiemobile/MainActivity.java
@@ -1,12 +1,8 @@
 package com.example.renadoparia.sportjunkiemobile;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,17 +16,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Log.d(TAG, "onCreate: Blah Blah");
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
