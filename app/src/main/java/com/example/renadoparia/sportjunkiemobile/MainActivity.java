3212ebf9aba64925242e2a7caf8fa5f69092ca23
renadope <renadoparia@gmail.com>
2017-02-25 14:29:59 -0400

    booya

--- app/src/main/java/com/example/renadoparia/sportjunkiemobile/MainActivity.java
+++ app/src/main/java/com/example/renadoparia/sportjunkiemobile/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Log.d(TAG, "onCreate: Hello World");
+        Log.d(TAG, "onCreate: Blah Blah");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
         {
